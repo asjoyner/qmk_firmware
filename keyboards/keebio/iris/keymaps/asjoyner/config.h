adeeb583cff9ce97b8958e1589a6f31ad9ffab70
Aaron S. Joyner <aaron@joyner.ws>
2020-04-28 18:03:58 -0400

    Don't override encoder resolution, it didn't help

--- keyboards/keebio/iris/keymaps/asjoyner/config.h
+++ keyboards/keebio/iris/keymaps/asjoyner/config.h
@@ -22,4 +22,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 //#define ENCODERS_PAD_A { B12 }
 //#define ENCODERS_PAD_B { B13 }
-#define ENCODER_RESOLUTION 15
+//#define ENCODER_RESOLUTION 15
